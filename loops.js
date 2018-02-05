502d2f3a5d15817ff2652eed6312fe4329311faf
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-05 00:19:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function doWhileLoop(arr) {
     return Math.random() >= 0.5
   }
   do {
-    delete arr[arr.length-1]
+    arr.unshift()
     return arr
   }
   while (arr.length>0 && maybeTrue());
